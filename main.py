3b8e98acefc9b7e2cc51d840b42b81bf7e6af3ff
John Paradise <john.paradise117@gmail.com>
2018-04-29 15:10:52 -0400

    Tried to make BS4 work.  Loopnet makes it hard for filters to work accurately with BS4.  Going to try Selenium next

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get('http://www.loopnet.com/for-sale/multifamily/?sk=63b914a1552119b05ef714d0276d7001&bb=np6wjo2t8S3z_ujn17uG&view=list&e=v')
+page = requests.get('http://www.loopnet.com/for-sale/multifamily/?sk=b12008959ee0e3323746b123877acb2b&view=list')
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
 links = soup.find_all('div', class_= 'listing-address-and-price')
 
-for a in links.find('a', href=True):
-    print (a['href'])
+elems = soup.select('#placardSec > div:nth-child(2) > div > div:nth-child(1) > div.gold > section.placard-details-container > div.placard-details > div > a')
 
+#Need to use selenium, not BS4
